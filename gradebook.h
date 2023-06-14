ad5a9aae2fd7f53ff1ed815e319814132d9cc461
SeanRCSC <112998933+SeanRCSC@users.noreply.github.com>
2023-06-14 12:37:19 -0400

    Update gradebook.h

--- gradebook.h
+++ gradebook.h
@@ -1,1 +1,28 @@
+#include <vector>
+#include <iostream>
+#include <iomanip>
+
+#ifndef GRADEBOOKPROJECT_GRADEBOOK_H
+#define GRADEBOOKPROJECT_GRADEBOOK_H
+
+
+class gradebook {
+private:
+    std::vector<int> grades;
+    std::vector<int> category;
+    std::vector<int> TotGrades;
+
+public:
+    gradebook();
+    //functions here
+    void infile(std::string fname);
+    void outfile(std::string fname);
+    float TotalGrade();
+
+
+
+};
+
+
+#endif //GRADEBOOKPROJECT_GRADEBOOK_H
 
