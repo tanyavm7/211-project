d14e9780ae60d3708a640c1212d7d1e12c22fd81
SeanRCSC <112998933+SeanRCSC@users.noreply.github.com>
2023-06-14 12:37:40 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
+#include <vector>
+#include <iostream>
+#include <iomanip>
+
+#include "gradebook.h"
+
+int main(int argc, char* argv[]) {
+    //create object Gradebook
+    gradebook Gradebook;
+
+    //CLA collects file name
+    std::string fname = argv[1];
+
+    //read through the Gradebook text file
+    Gradebook.infile(fname);
+
+    //get the total grade that the student currently has
+    std::cout<<"Welcome to your gradebook\nYour current grade as of now is: "<<std::endl;
+    Gradebook.TotalGrade();
+
+    //update Gradebook
+    Gradebook.outfile(fname);
+
+    return 0;
+}
 
