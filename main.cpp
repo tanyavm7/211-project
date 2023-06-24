3de90156f963e5562e3d00c16d3d09a1812da28b
tanyavm7 <90417982+tanyavm7@users.noreply.github.com>
2023-06-24 01:22:23 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,24 +17,21 @@ int main(int argc, char* argv[]) {
     //get the total grade that the student currently has
     int input;
     std::cout<<"Welcome to your grade book. "<<std::endl;
-    while(input != 5)
+    while(input != 6)
     {
         std::cout << "If you want to know your final grade in the class, please input 1." << std::endl;
         std::cout << "If you want to know a single grade in a specific category and assignment, please input 2." << std::endl;
         std::cout << "If you want to know your grades in a specific category, please input 3." << std::endl;
         std::cout << "If you want to change a grade in your grade book, please input 4." << std::endl;
-        std::cout << "If you finish with the grade book, please input 5." << std::endl;
+        std::cout << "If you want all grades for a category, please input 5." << std::endl;
+        std::cout << "If you finish with the grade book, please input 6." << std::endl;
 
         std::cin >> input;
 
         if(input == 1) // get final grade
         {
             std::cout << "Your final grade is..." << std::endl;
-            float grade;
-
-            gradebook.TotalGrade();
-
-            std::cout << grade << std::endl;
+            std::cout << gradebook.TotalGrade() << std::endl;
         }
         else if(input == 2) // get a specific grade
         {
@@ -46,36 +43,40 @@ int main(int argc, char* argv[]) {
             std::cin >> num;
             std::cout << "Your grade for " << category << " " << num << " is..." << std::endl;
 
-
-
-            std::cout << grade << std::endl;
+            gradebook.printGrade(num, category);
         }
         else if(input == 3) // get a specific category grade
         {
             std::string category;
-            std::cout << "Please input the category. For example, labs, assignments, projects." << std::endl;
+            std::cout << "Please input the category. For example, labs, assignments, project1, project2, final." << std::endl;
             std::cin >> category;
             std::cout << "Your grade for " << category << " is..." <<std::endl;
 
-
-
-            std::cout << grade << std::endl;
+            gradebook.getTotCategoryGrade(category);
         }
         else if(input == 4) {
             std::string category;
             int num;
-            float grade;
+            std::string newGrade;
             std::cout << "Please input the category. For example, labs, assignments, projects." << std::endl;
             std::cin >> category;
             std::cout << "Please input the number of category. For example, if you want to search lab 3, then input 3."
                       << std::endl;
             std::cin >> num;
             std::cout << "Please input the grade you want to change. For example, 25."
-            std::cin >> grade;
+            std::cin >> newGrade;
 
-
+            gradebook.changeGrade(category, num, newGrade);
         }
         else if(input == 5)
+        {
+            std::string category;
+            std::cout << "Please input the category. For example, labs, assignments, project1, project2, final." << std::endl;
+            std::cin >> category;
+
+            gradebook.printCatGrade(category);
+        }
+        else if(input == 6)
         {
             std::cout << "Thank you for using the grade book." << std::endl;
         }
